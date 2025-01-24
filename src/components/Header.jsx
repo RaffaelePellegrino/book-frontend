b706aaae3953019d3dd1f429e1d332036a7d0332
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-24 12:56:40 +0100

    navbar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
+import {Nav, NavLink} from "react-router-dom";
 
 function Header() {
-    return(
-        <h1>ciao sono l'header</h1>
+    return (
+        <section>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand">Navbar</NavLink>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </section>
     );
 }
 
