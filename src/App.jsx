c22a1d5c84d74ee70b07b426f3b3fe7502915df0
RaffaelePellegrino <waffleaffumicato@gmail.com>
2025-01-24 11:44:05 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from './pages/About';
+import defaultLayout from './pages/DefaultLayout';
+import Home from './pages/Home';
 
 
 function App() {
-  return (
-    <>
-      <h1>hello</h1>
-    </>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route Component={defaultLayout}>
+          <Route path='/' Component={Home}/>
+          <Route path='/about' Component={About}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
+export default App;
