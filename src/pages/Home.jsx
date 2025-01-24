5c2db4be86195b375a84785292d67672e612e9d9
giuse <giuseppedefilippo03@gmail.com>
2025-01-24 12:42:56 +0100

    adding card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
+import CardComponent from "../components/CardComponent"
+
+
 export default function Home() {
-    return(
-        <h1>sono Home</h1>
+    return (
+        <CardComponent />
     )
 }
