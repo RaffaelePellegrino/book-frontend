56e006c548648ad9c535002e802ff3a6a402a897
RaffaelePellegrino <waffleaffumicato@gmail.com>
2025-01-27 11:41:17 +0100

    changed route home with route books

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from './pages/About';
 import defaultLayout from './pages/DefaultLayout';
-import Home from './pages/Home';
+import Contact from "./pages/Contact";
 import Card from "./components/Card";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route Component={defaultLayout}>
-          <Route Index Component={Home}/>
+          <Route path='/' Component={Card}/>
           <Route path='/about' Component={About}/>
-          <Route path='/books'>
+          <Route path='/contact' Component={Contact}>
             <Route index Component={Card} />
             {/* <Route index path='/:id' /> */}
           </Route>
